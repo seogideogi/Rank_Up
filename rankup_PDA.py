fd140f4544a233da7a1859a2e34f9e0a4df68403
seogideogi <rhodalee78@gmail.com>
2025-01-13 12:25:09 +0900

    Add files via upload

--- rankup_PDA.py
+++ rankup_PDA.py
@@ -104,7 +104,11 @@ def initialize_components(selected_model):
 
 # Streamlit UI
 
-st.header("개인 채무 조정 Q&A 챗봇 💬 📚")
+st.header("개인 채무 조정 Q&A 챗봇 💬")
+st.subheader("1. 채무 및 연체정보")
+st.subheader("2. 자산")
+st.subheader("3. 소득")
+st.text(" 위 3가지 정보를 입력하시면, 상세한 답변을 얻으 실수 있습니다.")
 option = st.selectbox("Select GPT Model", ("gpt-4o-mini", "gpt-3.5-turbo-0125"))
 rag_chain = initialize_components(option)
 chat_history = StreamlitChatMessageHistory(key="chat_messages") # 스트림릿 상에서 사용자와 AI가 주고받은 대화가 Chat_messages라는 key값의 value로 저장 됨
